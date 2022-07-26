afc33e73ac4a7b17bb28d9d3bf3245927f3dc769
Md Arfan Khan <khan188993@gmail.com>
2022-07-26 16:39:35 +0600

    add isAuth into create,update and delete products

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,8 +4,8 @@ const { getAllProducts,createProducts, updateProducts, deleteProduct, getSingleP
 const { isAuthenticatedUser } = require('../middlewares/auth');
 const router = express.Router();
 
-router.route('/products').get(isAuthenticatedUser,getAllProducts);
-router.route('/products/new').post(createProducts);
-router.route('/products/:id').put(updateProducts).delete(deleteProduct).get(getSingleProduct)
+router.route('/products').get(getAllProducts);
+router.route('/products/new').post(isAuthenticatedUser,createProducts);
+router.route('/products/:id').put(isAuthenticatedUser,updateProducts).delete(isAuthenticatedUser,deleteProduct).get(getSingleProduct)
 
 module.exports = router;
